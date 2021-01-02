f2537e8983011e5a83562c33ee26b0632236c23c
dwinatech <diwina1993@hotmail.com>
2021-01-02 12:28:35 +0000

    Fetch properties from zoopla API

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+const App = () =>  {
+  const [properties, setProperties] = useState([]);
+
+  const fetchProperties = async () => {
+    const { data } = await axios.get(`http://api.zoopla.co.uk/api/v1/property_listings.json?postcode=g314dn&area=london&api_key=${process.env.REACT_APP_ZOOPLA_KEY}`)
+    setProperties(data)
+  }
+
+  useEffect(() => {
+    fetchProperties()
+  }, [])
+  console.log({ properties });
   return <h1>Dream House</h1>
 }
 
